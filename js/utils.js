7c2b96d2346afb269b0f2edafb494c4f7e6eaf71
Rachel Kim <yseo7045@gmail.com>
2023-08-13 22:51:29 -0400

    Fixed issues in utils.js

--- js/utils.js
+++ js/utils.js
@@ -2,11 +2,10 @@ Array.prototype.parse2D = function () {
     const rows = []
     for (let i = 0; i < this.length; i += 330) {
         rows.push(this.slice(i, i + 330))
-    for (let i = 0; i < this.length; i += 330) {
-        rows.push(this.slice(i, i + 330))
     }
     return rows
 }
+
 
 Array.prototype.createObjectsFrom2D = function () {
     const objects = []
@@ -15,8 +14,6 @@ Array.prototype.createObjectsFrom2D = function () {
             if (symbol === 23) {
                 objects.push(new CollisionBlock({
                     position: {
-                        x: x * 3,
-                        y: y * 3,
                         x: x * 3,
                         y: y * 3,
                     },
